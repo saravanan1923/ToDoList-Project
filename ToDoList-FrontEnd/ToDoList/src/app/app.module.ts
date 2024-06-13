c6b1f23f6d52ca4339e1d5a95f4f429ecae8188c
saravanan1923 <saravananstallone@gmail.com>
2024-06-13 21:31:48 +0530

    Implemented the login form

--- ToDoList-FrontEnd/ToDoList/src/app/app.module.ts
+++ ToDoList-FrontEnd/ToDoList/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,11 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LoginComponent]
 })
 export class AppModule { }
--- ToDoList-FrontEnd/ToDoList/src/app/login/login.component.ts
+++ ToDoList-FrontEnd/ToDoList/src/app/login/login.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  LoginForm!:FormGroup;
+  userName:any
+  password:any
+  success = false;
+  constructor(private formBuilder: FormBuilder, private loginService:LoginService) { }
+  ngOnInit(): void {
+    this.LoginForm = this.formBuilder.group({
+      userName:['',Validators.required],
+      password:['',Validators.required]
+    })
+  }
+
+  onSubmit(){
+   this.loginService.login().subscribe((data:any)=>{
+      console.log("This is data from DB", data)
+      console.log(this.LoginForm.value.userName)
+      console.log(this.LoginForm.value.password)
+      if(data[0].userName===this.LoginForm.value.userName && data[0].password===this.LoginForm.value.password){
+        alert("Login Successful")
+      }
+      else{
+        alert("Login Failed")
+      }
+    })
+  }
 
 }
